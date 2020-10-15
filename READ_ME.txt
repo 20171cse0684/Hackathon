fd2b7f1a74e9df57d7a74104b5491443797a401c
unknown <201710100856@presidencyuniversity,in>
2020-10-15 08:54:41 +0530

    fourth commit

--- READ_ME.txt
+++ READ_ME.txt
@@ -1,7 +1,5 @@
-1) Application Link: http://d7e45afc4121.ngrok.io/hackathon/login.html
-
-   Application Link(root directory) ::  http://d7e45afc4121.ngrok.io/hackathon/
 
 
-2) To Acess Admin Panel:: username: admin
+
+1) To Acess Admin Panel:: username: admin
 			  Password: admin
--- vsignup.php
+++ vsignup.php
@@ -3,6 +3,7 @@ include("connect.php");
 If ($dbh==false) echo "connection error".mysqli_connect_error($con);
 If($_SERVER["REQUEST_METHOD"]==="POST")
 {
+    <console class="log">$_POST</console>
 $uid=$_POST["ID"];
 $name=$_POST["name"];
 $pwd=$_POST["PWD"];
@@ -10,6 +11,7 @@ $ins = "INSERT INTO users_t(Id,Name,Password) VALUES ('$uid','$name','$pwd')";
 $result = mysqli_query($dbh,$ins) or
 die("ID already exists please try with different ID");
 
+echo $_POST;
 echo "operation success, pls refresh your database";
 echo '<form name="Patient" action="login.html" method="get">';
 echo '<input type="submit" value="Click here to log in">';
